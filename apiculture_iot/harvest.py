05b8528d607ba550aa76af0c1bb1c9ad46d5b908
Al Razelle A. Ramores <al.razelle.a.ramores@gmail.com>
2026-01-02 21:32:19 +0800

    testing servo

--- apiculture_iot/harvest.py
+++ apiculture_iot/harvest.py
@@ -263,7 +263,7 @@ def handle_needle_servo_angle(data):
 
         needle_servo = AngularServo(NEEDLE_SERVO_PIN, min_angle=-90, max_angle=90, initial_angle=None)
         needle_servo.angle = 90
-        time.sleep(round(angle / 200, 2))
+        time.sleep(round(angle / 270, 2))
         needle_servo.angle = None
         needle_servo.close()
         time.sleep(5)
