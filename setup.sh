0daf7503313054f5949d5a2fd1ccc1af0f95aead
Al Razelle A. Ramores <al.razelle.a.ramores@gmail.com>
2026-01-06 01:01:39 +0800

    setup script

--- setup.sh
+++ setup.sh
@@ -3,11 +3,11 @@
 # Update and upgrade the system packages
 sudo apt update
 
-sudo apt install python3.11-dev
-sudo apt install python3-setuptools python3-wheel
+sudo apt install python3.11-dev -y
+sudo apt install python3-setuptools python3-wheel -y
 sudo apt install python3-picamzero -y
 sudo apt install python3-gpiozero -y
-sudo apt install rpi-connect
+#sudo apt install rpi-connect -y # causing ssh failure
 
 # install desktop
 #sudo apt install raspberrypi-ui-mods lightdm xserver-xorg arandr -y
@@ -21,8 +21,12 @@ source ~/.bashrc
 mkdir -p /home/apiculture/py_env
 
 # Create and activate the BME280 virtual environment
+# rm -rf /home/apiculture/py_env/bme280_venv
+# pip cache purge
+# rm -rf ~/.cache/pip
 python3 -m venv /home/apiculture/py_env/bme280_venv
 source /home/apiculture/py_env/bme280_venv/bin/activate
+pip3 install --upgrade pip setuptools wheel
 
 # Re-install system packages (as specified, even if redundant)
 pip3 install RPi.GPIO
@@ -32,6 +36,7 @@ pip3 install adafruit-blinka
 pip3 install adafruit-circuitpython-busdevice
 pip3 install adafruit-circuitpython-bme280
 
-pip3 install -r /home/apiculture/apiculture-iot/requirements.txt
+# pip3 install -r /home/apiculture/apiculture-iot/requirements.txt
+pip3 install flask flask-socketio python-socketio gpiozero picamera2 requests
 
 echo "Setup complete! Virtual environment 'bme280_venv' is activated. Run 'deactivate' to exit when done."
