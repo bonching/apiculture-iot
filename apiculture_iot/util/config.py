b1a9c9c50aa4fc3dae7d967bd8019709fe362501
Al Razelle A. Ramores <al.razelle.a.ramores@gmail.com>
2026-02-01 16:02:47 +0800

    updated beehive_id

--- apiculture_iot/util/config.py
+++ apiculture_iot/util/config.py
@@ -4,7 +4,7 @@ API_HOST = os.environ.get('API_HOST', '192.168.68.104')
 API_PORT = 8081
 MONGODB_URL = f'mongodb://{API_HOST}:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=apiculture'
 
-BEEHIVE_ID = '693ad7c84739d5289a1e0834'
+BEEHIVE_ID = '693ad7c84739d5289a1e0835'
 
 DATA_COLLECTION_METRICS = {
     'temperature': { 'base_value': 34.5, 'variance': 2, 'unit': '°C', 'anomaly_rate': 3 },
